cc717e560d7a3924fb18df9dd74a6ed15b47f9b5
Your Name <you@example.com>
2022-06-27 20:05:09 +0530

    uc2 is implemented

--- src/workshop1/gambler.java
+++ src/workshop1/gambler.java
@@ -5,7 +5,12 @@ public class gambler {
 	public static void main(String[] args) {
 		int stake = 100;
 		int bet = 1;
-
+		float win = (float)(Math.random());
+		stake -= 1;
+		if(win>0.5) {
+			stake += (bet*2);
+		}
+		System.out.println(+stake);
 	}
 
 }
