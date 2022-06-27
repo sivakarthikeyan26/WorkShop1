4722bebbce32fe99846237dfdcd9c38da7e47402
Your Name <you@example.com>
2022-06-27 21:15:11 +0530

    for 20 days in implemented

--- src/workshop1/gambler.java
+++ src/workshop1/gambler.java
@@ -5,18 +5,34 @@ public class gambler {
 	public static void main(String[] args) {
 		int stake = 100;
 		int bet = 1;
+		int day_count=20;
 		boolean flag=false;
-		while(flag!=true) {
-			float win = (float)(Math.random());
-			stake -= 1;
-			if(win>0.5) {
-				stake += (bet*2);
+		for (int i=1; i<=20; i++) {
+			flag=false;
+			while(flag!=true) {
+				float win = (float)(Math.random());
+				day_count+=1;
+				stake -= 1;
+				if(win>0.5) {
+					stake += (bet*2);
+				}
+				if(stake == 50 || stake ==150 || stake==25 || stake==13) {
+					flag=true;
+					System.out.println("The player has retired with "+stake+" for day "+i);
+				}
 			}
-			if(stake == 50 || stake ==150) {
-				flag=true;	
+			if(stake<1) {
+				System.out.println("The gambler has lost all his money");
+				break;
 			}
+			
 		}
-		System.out.println("The gambler has retired for the day with "+stake+" stake");
+		if(stake<100) {
+			System.out.println("The player has lost "+(100-stake)+" in 20 days");
+		}
+		else {
+			System.out.println("The player has won "+(stake-100)+" in 20 days");
+		}
 	}
 
 }
