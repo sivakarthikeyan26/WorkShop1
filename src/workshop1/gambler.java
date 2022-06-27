489e5bafdd23254c1a7ea895e0be553d9a78c82a
Your Name <you@example.com>
2022-06-27 20:35:30 +0530

    gambler retire in 50% of win or loose stake is implemented

--- src/workshop1/gambler.java
+++ src/workshop1/gambler.java
@@ -5,12 +5,18 @@ public class gambler {
 	public static void main(String[] args) {
 		int stake = 100;
 		int bet = 1;
-		float win = (float)(Math.random());
-		stake -= 1;
-		if(win>0.5) {
-			stake += (bet*2);
+		boolean flag=false;
+		while(flag!=true) {
+			float win = (float)(Math.random());
+			stake -= 1;
+			if(win>0.5) {
+				stake += (bet*2);
+			}
+			if(stake == 50 || stake ==150) {
+				flag=true;	
+			}
 		}
-		System.out.println(+stake);
+		System.out.println("The gambler has retired for the day with "+stake+" stake");
 	}
 
 }
